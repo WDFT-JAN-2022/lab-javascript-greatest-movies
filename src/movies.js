bdf921f3d7317c5e148d07bdd57e5367d2984987
Mihir Patel <mihirkpatel9702@gmail.com>
2022-01-13 17:52:36 -0500

    iteration 6

--- src/movies.js
+++ src/movies.js
@@ -91,7 +91,7 @@ function orderByYear(movies) {
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(arr) {return arr.map(e => e.title).sort().slice(0,20);}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
